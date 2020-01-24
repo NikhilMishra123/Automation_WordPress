bd5fe179f664c17e234bca2f35fc402590335bb5
Nikhil <nikhil_cse16@nitsri.net>
2020-01-24 14:14:14 +0530

    comments removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,3 @@
 class User < ApplicationRecord
 	has_many :sites
-	
-	#nikhil for testing purpose  ::: remove 
-	 def self.save_result(results, test_id_to_name, site_URL)
-		 User.create(
-			 :email => 'nikhil@gmail.com',
-			 :password => 'nikhil'
-		 );
-  end
-
-	#nikhil for testing purpose ::: remove	
-	 def self.retriveUser()
-		 return User.where( id: '1')
-	 end
-
 end
